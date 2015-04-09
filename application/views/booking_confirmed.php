94101f273db012fae959d6bbf28d019caeac0d65
Koruba <tilmanzoellner@web.de>
2015-04-09 10:12:55 +0200

    Änderung der Ansichts-Views in Tabellenform

--- application/views/booking_confirmed.php
+++ application/views/booking_confirmed.php
@@ -1,5 +1,5 @@
 <h1>Erfolg!</h1>
 <p>Ihr Kurs wurde erfolgreich gebucht.</p>
 <form>
-<input type="button" value="Zurueck zur Uebersicht" onclick="window.location.href='http://localhost:8080/Nachhilfe_Test/index.php/course_controller/overview'">
+<input type="button" value="Zurueck zur Uebersicht" onclick="window.location.href='http://localhost/Nachhilfe_Test/index.php/course_controller/overview'">
 </form>
