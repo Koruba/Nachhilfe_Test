35a2743bc72d96fa2bd6fb228a841897e3fa2472
Koruba <tilmanzoellner@web.de>
2015-04-09 10:14:13 +0200

    Änderung der Ansichts-Views in Tabellenform
    Dieser und die letzten beiden Commits sollten gemeinsam hochgeladen
    werden. Leider ist ein Fehler aufgetreten und ich musste drei
    verschiedene daraus machen.

--- application/views/course_details.php
+++ application/views/course_details.php
@@ -1,12 +1,15 @@
-<?php
-	//error_reporting(0);
-	echo "Hallo Welt".'<br />';
-	echo $course_detail['queryUserFirstName'].'<br />';
-	echo $course_detail['queryUserLastName'].'<br />';
-	echo $course_detail['queryCourseSubject'];
-	//echo $course_detail['queryCourseNo'].'<br />';
-	//echo $course_detail['Instructor_No'];
-?>
+<table>
+	<tr>
+		<td>Anbieter Vorname</td>
+		<td>Anbieter Nachname</td>
+		<td>Fach</td>
+	</tr>
+	<tr>
+		<td><?php echo $course_detail['queryUserFirstName']; ?></td>
+		<td><?php echo $course_detail['queryUserLastName']; ?></td>
+		<td><?php echo $course_detail['queryCourseSubject']; ?></td>
+	</tr>
+</table>
 <form>
-<input type="button" value="Buchen" onclick="window.location.href='http://localhost:8080/Nachhilfe_Test/index.php/course_controller/confirmation/<?php echo$course_detail['queryCourseNo'] ?>'">
+<input type="button" value="Buchen" onclick="window.location.href='http://localhost/Nachhilfe_Test/index.php/course_controller/confirmation/<?php echo$course_detail['queryCourseNo'] ?>'">
 </form>
