cc728d929e6f214f45d53ebedf858f32ab4ba7b3
Koruba <tilmanzoellner@web.de>
2015-04-15 15:40:33 +0200

    Design des Navigationsbereichs angepasst
    An Vorgaben des Templates

--- application/views/templates/navigation_template.php
+++ application/views/templates/navigation_template.php
@@ -1,8 +1,32 @@
-<table>
-	<tr>
-		<td><a href="<?php echo base_url().'index.php/course'; ?>">Start</a></td>
-		<td><a href="<?php echo base_url().'/index.php/user/login'; ?>">Login</a></td>
-		<td><a href="<?php echo base_url().'/index.php/user/logout'; ?>">Logout</a></td>
-		<td><a href="<?php echo base_url().'/index.php/user/register'; ?>">Registrieren</a></td>
-	</tr>
-</table>
+<li>
+    <a class="w active" href="<?php echo base_url().'index.php/course'; ?>">Start</a>
+</li>
+<?php
+if ($this->session->userdata('login') != TRUE)
+{
+?>
+	<li>
+		<a class="w" href="<?php echo base_url().'/index.php/user/login'; ?>">Login</a>
+	</li>
+<?php
+}
+
+if ($this->session->userdata('login') == TRUE)
+{
+?>
+	<li>
+		<a class="w" href="<?php echo base_url().'/index.php/user/logout'; ?>">Logout</a>
+	</li>
+<?php
+}
+
+if ($this->session->userdata('login') != TRUE)
+{
+?>
+	<li>
+		<a class="w" href="<?php echo base_url().'/index.php/user/register'; ?>">Registrieren</a>
+	</li>
+<?php
+}
+?>
+::after
